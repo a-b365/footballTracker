ab4b8eabb068c4c482e033fc1f6a333ac748c38f
Amir Bhattarai <alphaelite10@gmail.com>
2021-11-19 14:23:11 +0545

    Managing static files

--- Projectfootball/settings.py
+++ Projectfootball/settings.py
@@ -144,10 +144,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,"static/"),
-    )
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
